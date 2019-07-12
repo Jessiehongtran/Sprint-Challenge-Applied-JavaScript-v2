5a7584c4e0b3a516270906f9adce9933b9c6a5a3
Your Name <you@example.com>
2019-07-12 11:45:59 -0400

    created tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,36 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector('.topics')
+// console.log(topics)
+
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+.then(res =>{
+    console.log('show data',res)
+    const getTab = res.data.topics
+    // console.log(getTab)
+    getTab.forEach(tabs => {
+        const showTab = createTab(tabs)
+        console.log(getTab)
+        topics.appendChild(showTab) 
+    }
+
+    )
+})
+
+.catch(
+    error => {
+        // Handles failure:
+        console.log('The dogs API is currently down, try again later', error)
+    }
+)
+
+function createTab(){
+    const tabs = document.createElement('div')
+    console.log(tabs)
+    tabs.classList.add('tab')
+    tabs.textContent = "topic here"
+    return tabs
+}
+
